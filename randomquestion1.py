f4cd8f41fc84c7c6850e6914ab24e82b5de49856
James <4whalj16@solent.ac.uk>
2019-01-27 17:35:58 +0000

    Continued work on recreating randomquestion.py to incorporate classes.

--- randomquestion1.py
+++ randomquestion1.py
@@ -1,17 +1,35 @@
 from random import randint as rint # gave randint an alias to prevent me from having to type random.randint every time.
 
-class mcqtemplates:
+class qtemplate:
     def __init__(self):
-        global qbuffer, count   # might not need.
         self.qbuffer = []
-        #qpairs = []   # what did I need this for?
         self.count = 0
 
     def fillbuffer(self,choices=2,rstart=1,rend=10):
         for x in range(choices):
             self.qbuffer.append(rint(rstart,rend))
         
-o = mcqtemplates
-o()
+class scquestion(qtemplate):
+    def __init__(self):
+        super().__init__()
+        self.errorcount = ()
+        self.uanswer = ()
+        self.fillbuffer()
 
-print(o.qbuffer)
+    def singlechoicequestion(self):
+        try:
+            while self.errorcount != 3:
+                self.fillbuffer()
+                self.uanswer = input("What is {} + {}? ".format(self.qbuffer[0],self.qbuffer[1]))
+                if int(self.uanswer) == self.qbuffer[0] + self.qbuffer[1]:
+                    print(self.uanswer,"is correct.")
+                else:
+                    print("Incorrect. The correct answer was:",self.qbuffer[0] + self.qbuffer[1])
+                    self.errorcount += 1
+        except KeyboardInterrupt as ki:
+            print("Thanks for playing!")
+
+# Automatically runs the program on start.
+if __name__ == "__main__":
+    quest = scquestion()
+    quest.singlechoicequestion()
