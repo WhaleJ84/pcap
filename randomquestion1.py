517fa478a08938f5848411decfca2ca4e8bd2059
James <4whalj16@solent.ac.uk>
2019-01-28 21:56:12 +0000

    Continue work

--- randomquestion1.py
+++ randomquestion1.py
@@ -3,7 +3,7 @@ from random import randint as rint # gave randint an alias to prevent me from ha
 class qtemplate:
     def __init__(self):
         self.count = 0
-        self.values = 9
+        self.values = 9 # stops working correctly when higher than 9
         #print("Debug 0")
 
     def fillbuffer(self,values,rstart=1,rend=10):
@@ -14,7 +14,7 @@ class qtemplate:
             tuple1 = (rint(rstart,rend),)
             self.qbuffer += tuple1
             #print("Debug 1",self.qbuffer)
-        for x in range(len(self.qbuffer)):
+        for x in range(len(self.qbuffer)): # Can be optimised by finding way to convert existing tuple into a new list.
             self.qbtlist.append(self.qbuffer[x])
             self.qbtotal += self.qbuffer[x]
         print("Debug 5",self.qbtlist,self.qbtotal)
@@ -33,11 +33,11 @@ class scquestion(qtemplate):
                 print("\nWhat is {} ".format(self.qbuffer[0]),end="")
                 for x in range(self.values - 1):
                     print("+ {} ".format(self.qbuffer[(x + 1)]),end="")
-                self.uanswer = input()
+                self.uanswer = input("\n")
                 if int(self.uanswer) == self.qbtotal:
                     print("correct.")
                 else:
-                    print("Incorrect. The correct answer was:",self.qbuffer[0] + self.qbuffer[1])
+                    print("Incorrect. The correct answer was:",self.qbtotal)
                     self.errorcount += 1
                 self.fillbuffer(self.values)
                 #print("Debug 4")
