6245bb1ea6d26f9a9a2ca651aa74e8f37ed8a204
James <4whalj16@solent.ac.uk>
2019-02-01 21:55:24 +0000

    Continued work on the operators

--- randomquestion1.py
+++ randomquestion1.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python3
+
 from random import randint as rint 
 import operator as o
 
 class qtemplate:
     def __init__(self):
-        self.count = 0
         self.values = 3 # stops working correctly when higher than 9
-        self.op = tuple(input("Please enter {} operators without spaces, else ENTER for defaults.\n+,-,*,/,//\n".format(self.values-1)))
-        #print("Debug 0")
+        self.randop = ("+","-","*","/","%")
+        #self.specop = tuple(input("""Please enter {} operators without spaces, else ENTER for defaults.
+#+ addition
+#- subtraction
+#* multiplication
+#/ whole division
+#% integer division\n""".format(self.values-1)).replace(" ", ""))
+        #print("Debug qtemplate")
 
     def fillbuffer(self,values,rstart=1,rend=10):
         self.qbuffer = tuple()
@@ -15,34 +22,32 @@ class qtemplate:
         for x in range(values):
             tuple1 = (rint(rstart,rend),)
             self.qbuffer += tuple1
-            #print("Debug 1",self.qbuffer)
+            #print("Debug fillbuffer 1:",self.qbuffer)
         for x in range(len(self.qbuffer)): # Can be optimised by finding way to convert existing tuple into a new list.
             self.qbtlist.append(self.qbuffer[x])
             if x == 0:
                 self.qbtotal = self.qbuffer[x]
-                #print("Debug 5",self.qbtotal) 
+                #print("Debug fillbuffer 2a:",self.qbtotal) 
             else:
-                print("Debug",x,self.op)
+                #print("Debug fillbuffer 2b:",x,self.op)
                 self.operation(x-1)
                 self.qbtotal = self.oper(self.qbtotal,self.qbuffer[x])
-            #print("Dedug 6", self.qbtotal)
-        print("Debug 7",self.qbtlist,self.qbtotal)
+        #print("Debug fillbuffer 2c:",self.qbtlist,self.qbtotal)
 
     def operation(self,opx=0): 
+        self.op = tuple(self.randop)
         if self.op[opx] == "+":
             self.oper = o.add
         elif self.op[opx] == "-":
             self.oper = o.sub
         elif self.op[opx] == "*":
             self.oper = o.mul
-        elif self.op[opx] == "/": # refine function to only require 1 decimal point.
+        elif self.op[opx] == "/":
             self.oper = o.truediv
-        elif self.op[opx] == "//":
+        elif self.op[opx] == "%":
             self.oper = o.floordiv
-        else:
-            self.op = "+"
-            self.oper = o.add
-        print("Debug:",self.op)
+        else: print("Debug operation else:",self.op)
+        #print("Debug operation:",self.op)
 
 class scquestion(qtemplate):
     def __init__(self):
@@ -51,7 +56,7 @@ class scquestion(qtemplate):
         self.uanswer = ()
         self.operation()
         self.fillbuffer(self.values)
-        #print("Debug 2")
+        #print("Debug scquestion")
 
     def singlechoicequestion(self):
         while self.errorcount != 3:
@@ -60,13 +65,13 @@ class scquestion(qtemplate):
                 for x in range(self.values - 1):
                     print("{} {} ".format(self.op[x],self.qbuffer[(x + 1)]),end="")
                 self.uanswer = input("\n")
-                if float(self.uanswer) == self.qbtotal:
+                if float(self.uanswer) == round(self.qbtotal,2):
                     print("correct.")
                 else:
                     print("Incorrect. The correct answer was:",self.qbtotal)
                     self.errorcount += 1
                 self.fillbuffer(self.values)
-                #print("Debug 4")
+                #print("Debug singlechoicequestion")
             except KeyboardInterrupt as ki:
                 break
             except ValueError as ve:
