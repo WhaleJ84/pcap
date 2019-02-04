1ae9356abf843594c37107de8aea03de64f67d0d
James <4whalj16@solent.ac.uk>
2019-02-04 00:40:30 +0000

    Refined randop functionality.

--- randomquestion.py
+++ randomquestion.py
@@ -1,61 +1,90 @@
-import random
+#!/usr/bin/env python3
 
-def multiplechoicequestiontemplate(num_of_choices=2, rand_start=1, rand_end=10):
-# NOTE: Starting with lists as the guestionbuffer. Later on with better knowledge, change to tuple to allow more complex values(e.g. floats, strings).
-# NOTE: May alo need to change randint to provide things like floats and strings if possible.
+from random import randint as rint 
+import operator as o
 
-    global questionbuffer, count #, num_of_choices, rand_end # removed to allow function parameter arguements to work
-    questionbuffer = []
-    questionpairs = []
-    count = 0
-    #operatorlist = [+, -, *, /, %, =, ==, !, ~, ++. --, <<, >>, <, <=, >, >=, !=, |, &&, ||, +=, -+, *=, /=, %=, &=, ^=, |=, >>=, <<=] # Doesn't work
-    #num_of_choices = 2 #\
-    #rand_start = 1 #----| removed in favour of function parameter arguements instead
-    #rand_end = 10 #-----/
+class qtemplate:
+    def __init__(self):
+        self.values = 3 # stops working correctly when higher than 9
+        self.randop = ("+","-","*","/","%")
+        #self.specop = tuple(input("""Please enter {} operators without spaces, else ENTER for defaults.
+#+ addition
+#- subtraction
+#* multiplication
+#/ whole division
+#% integer division\n""".format(self.values-1)).replace(" ", ""))
+        self.ophist = tuple()
+        #print("Debug qtemplate")
 
-    for x in range(num_of_choices):
-        questionbuffer.append(random.randint(rand_start,rand_end))
-        if len(questionbuffer) % 2 == 0: # odd numbers
-            while questionbuffer[0+count+1]!= questionbuffer[-1]:
-                questionpairs.append(questionbuffer[0+count], questionbuffer[0+count+1])
-                count += 1
-
-        else: # even numbers
-            while questionbuffer[0]+count != questionbuffer[-1]:
-                    questionpairs.append(questionbuffer[0]+count)
-                    questionpairs.append(questionbuffer[0]+count+1)
-                    count += 1		
+    def fillbuffer(self,values,rstart=1,rend=10):
+        self.qbuffer = tuple()
+        self.qbtlist = []
+        self.qbtotal = 0
+        for num in range(values):
+            tuple1 = (rint(rstart,rend),)
+            self.qbuffer += tuple1
+            #print("Debug fillbuffer 1:",self.qbuffer)
+        for num in range(len(self.qbuffer)): # Can be optimised by finding way to convert existing tuple into a new list.
+            self.qbtlist.append(self.qbuffer[num])
+            if num == 0: # first loop, creates list
+                self.qbtotal = self.qbuffer[num]
+                #print("Debug fillbuffer 2a:",self.qbtotal) 
             else:
-                    questionpairs.append(questionbuffer[0]+count)
-    #print(questionbuffer,questionpairs,count) # for debugging
+                #print("Debug fillbuffer 2b:",num,self.op)
+                self.opnum = rint(0, (len(self.randop)-1))
+                self.operation(self.opnum)
+                self.qbtotal = self.oper(self.qbtotal,self.qbuffer[num])
+        #print("Debug fillbuffer 2c:",self.qbtlist,self.qbtotal)
+        print("Debug CHEAT:",self.qbtotal)
 
-# NOTE: Doesn't seem to be adding the 2nd integer to questionbuffer
+    def operation(self,opnum=0): 
+        self.op = tuple(self.randop)
+        if self.op[opnum] == "+":
+            self.oper = o.add
+        elif self.op[opnum] == "-":
+            self.oper = o.sub
+        elif self.op[opnum] == "*":
+            self.oper = o.mul
+        elif self.op[opnum] == "/":
+            self.oper = o.truediv
+        elif self.op[opnum] == "%":
+            self.oper = o.floordiv
+        else: print("Debug operation else:",self.op)
+        self.ophist += tuple(self.op[opnum],)
+        #print("Debug operation:",self.op)
 
-def multiplechoicequestion(): #---------------------------------------------------------\<- that didn't work
-    errorcount = 0                                                                     #|
-    #useranswer = "" #----------|created just to allow multiplechoicequestion() to loop |
-    #while useranswer != -999: #|
-    try:
-        while errorcount != 3:                                                             #|
-            multiplechoicequestiontemplate() #----------------------------------------------/find a way to link them
-            useranswer = int(input("What is: {} + {}? ".format(questionbuffer[0],questionbuffer[1]))) # find a way to specify the operator and facilitate larger numebr of integers
-            if useranswer == questionbuffer[0] + questionbuffer[1]:                                    # find a way to specify the operator 
-                print(useranswer,"is Correct!")
-            else:
-                print("Incorrect. The correct answer was:", questionbuffer[0] + questionbuffer[1])     # find a way to specify the operator
-                errorcount += 1
-    except KeyboardInterrupt as ki:
-        print("Thanks for playing!")
+class scquestion(qtemplate):
+    def __init__(self):
+        super().__init__()
+        self.errorcount = 0
+        self.uanswer = ()
+        self.operation()
+        self.fillbuffer(self.values)
+        #print("Debug scquestion")
 
-multiplechoicequestion()
+    def singlechoicequestion(self):
+        while self.errorcount != 3:
+            try:
+                print("\nWhat is {} ".format(self.qbuffer[0]),end="")
+                for num in range(self.values - 1):
+                    print("{} {} ".format(self.ophist[num],self.qbuffer[(num + 1)]),end="")
+                self.uanswer = input("\n")
+                if float(self.uanswer) == round(self.qbtotal,1):
+                    print("Correct.")
+                else:
+                    print("Incorrect. The correct answer was:",self.qbtotal)
+                    self.errorcount += 1
+                self.ophist = tuple()
+                self.fillbuffer(self.values)
+                #print("Debug singlechoicequestion")
+            except KeyboardInterrupt as ki:
+                break
+            except ValueError as ve:
+                print("{} is not a valid answer.".format(self.uanswer))
+                self.errorcount += 1
+        print("\nThanks for playing!")
 
-# idea behind num_of_choices change:
-#   always have num_of_choices[-1] be the end
-#   for odd num_of_choices length:
-#       have sections of 2 entries (e.g. num_of_choices[0], num_of_choices[1]) be input as  num_of_choices[-1] will correctly finish it
-#   for even num_of_choices lenth:
-#       have an if statement comparing final entry with num_of_choices[-1], if true, ?delete final entry?
-
-# idea behind the operator:
-# ???
-
+# Automatically runs the program on start.
+if __name__ == "__main__":
+    quest = scquestion()
+    quest.singlechoicequestion()
